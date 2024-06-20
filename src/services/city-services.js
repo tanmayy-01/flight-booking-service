5e4d59d893670afab61e95d7c38f6146f91b6d03
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-06-20 23:54:46 +0530

    Error message is added for duplicate city name entry

--- src/services/city-services.js
+++ src/services/city-services.js
@@ -9,7 +9,7 @@ async function createCity(data) {
         const city = await cityRepository.create(data);
         return city;
     } catch (error) {
-        if(error.name == 'SequelizeValidationError') {
+        if(error.name == 'SequelizeValidationError' || error.name == 'SequelizeUniqueConstraintError') {
             let explanation = [];
             error.errors.forEach((err) => {
                 explanation.push(err.message)
