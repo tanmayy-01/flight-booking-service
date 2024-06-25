02d1591f1c566a1bbe5b8a8658ddf644fac16584
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-06-25 22:15:21 +0530

    Added createFlight service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     airplaneService: require('./airplane-services'),
     cityService: require('./city-services'),
-    airportService: require('./airport-services')
+    airportService: require('./airport-services'),
+    flightServices: require('./flight-services')
 }
